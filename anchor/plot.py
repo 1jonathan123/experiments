02a0a854e7239bd83104bbcd02946240b9f9601a
Your Name <you@example.com>
2023-01-24 15:12:50 +0200

    minor changes

--- anchor/plot.py
+++ anchor/plot.py
@@ -1,32 +1,29 @@
-from llm_evaluator.plot import get_xa, get_average_xy, get_greedy_answer, get_average_distance, get_average_error
+from llm_evaluator.plot import get_xa, get_average_xy, get_greedy_answer, get_average_distance, get_average_error, \
+    IgnoreThisException
 import matplotlib.pyplot as plt
 import os, sys
+from functools import partial
 
 
 def da_to_xy(data, answer):
     return data[-1], int(get_greedy_answer(answer))
 
 
-def get_xy(directories, directory):
+def get_xy(directories, directory, func=da_to_xy, label=None):
+    if label is None:
+        label = directory
+
     xa = {}
 
     for d in directories:
-        xa = get_xa(os.path.join(d, directory), da_to_xy, xa)
+        xa = get_xa(os.path.join(d, directory), func, xa)
 
-    distance = 0
-    denominator = 0
-
-    for x in xa:
-        denominator += len(xa[x])
-        for y in xa[x]:
-            distance += y - x
-
-    print(f'{directory}: {get_average_distance(xa)} {get_average_error(xa)}')
+    print(f'{label}: {get_average_distance(xa)} {get_average_error(xa)}')
 
     return get_average_xy(xa)
 
 
-def main():
+def plot_average():
     plt.plot(list(range(30, 70)), list(range(30, 70)), color='green', label='true value')
 
     plt.plot(*get_xy(sys.argv[1:], 'small'), color='red', label='small examples')
@@ -38,6 +35,34 @@ def main():
 
     plt.legend()
     plt.show()
+
+
+def da_to_xy_large(index, data, answer):
+    if data[index] <= 70:
+        raise IgnoreThisException
+
+    return data[-1], int(get_greedy_answer(answer))
+
+
+def da_to_xy_small(index, data, answer):
+    if data[index] >= 30:
+        raise IgnoreThisException
+
+    return data[-1], int(get_greedy_answer(answer))
+
+
+def plot_with_respect_to_last():
+
+    for i in range(4):
+        get_xy(sys.argv[1:], 'balanced', partial(da_to_xy_large, i), f'{i} large')
+        get_xy(sys.argv[1:], 'balanced', partial(da_to_xy_small, i), f'{i} small')
+
+    get_xy(sys.argv[1:], 'balanced')
+
+
+def main():
+    plot_average()
+    #plot_with_respect_to_last()
 
 
 if __name__ == "__main__":
